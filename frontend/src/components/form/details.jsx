5124075270eb5554e0bd247d2dc9964481b2d3de
kumariTanya3 <tanya.edo3@gmail.com>
2026-07-21 18:58:56 +0530

    more features

--- frontend/src/components/form/details.jsx
+++ frontend/src/components/form/details.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateInteraction } from "../../redux/slices/interaction";
+
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TimePicker from "react-time-picker";
@@ -6,8 +8,8 @@ import "react-time-picker/dist/TimePicker.css";
 import "react-clock/dist/Clock.css";
 
 function Details() {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedTime, setSelectedTime] = useState("09:00");
+  const dispatch = useDispatch();
+  const interaction = useSelector((state) => state.interaction);
 
   return (
     <div className="rounded-lg border border-gray-200 bg-white p-5 shadow-sm">
@@ -16,26 +18,28 @@ function Details() {
       </h2>
 
       <div className="grid grid-cols-2 gap-4">
-
+        {/* HCP Name */}
         <div>
           <label className="mb-2 block text-sm font-medium">
             HCP Name
           </label>
-
           <input
             type="text"
+            value={interaction.hcpName || ""}
             readOnly
-            placeholder="AI will populate..."
+            placeholder="Search or Select HCP..."
             className="w-full rounded-lg border bg-gray-50 p-3"
+            //className="w-full rounded-lg border bg-gray-50 p-3 cursor-not-allowed" //if we want to cursor make no changes
           />
         </div>
 
+        {/* Interaction Type */}
         <div>
           <label className="mb-2 block text-sm font-medium">
             Interaction Type
           </label>
-
           <select
+            value={interaction.interactionType || "Meeting"}
             disabled
             className="w-full rounded-lg border bg-gray-50 p-3"
           >
@@ -46,14 +50,22 @@ function Details() {
           </select>
         </div>
 
+        {/* Date */}
         <div>
           <label className="mb-2 block text-sm font-medium">
             Date
           </label>
-
           <DatePicker
-            selected={selectedDate}
-            onChange={setSelectedDate}
+            selected={
+              interaction.date ? new Date(interaction.date) : null
+            }
+            onChange={(date) => {
+              dispatch(
+                updateInteraction({
+                  date: date ? date.toLocalDateString("en-CA") : "",
+                })
+              );
+            }}
             dateFormat="dd/MM/yyyy"
             className="w-full rounded-lg border bg-gray-50 p-3"
             showMonthDropdown
@@ -62,51 +74,63 @@ function Details() {
           />
         </div>
 
+        {/* Time */}
         <div>
           <label className="mb-2 block text-sm font-medium">
             Time
           </label>
-
           <TimePicker
-            onChange={setSelectedTime}
-            value={selectedTime}
+            onChange={(time) => {
+              dispatch(
+                updateInteraction({
+                  time,
+                })
+              );
+            }}
+            value={interaction.time || ""}
+            //disabled
             disableClock={false}
             clearIcon={null}
           />
         </div>
       </div>
 
+      {/* Attendees */}
       <div className="mt-5">
         <label className="mb-2 block text-sm font-medium">
           Attendees
         </label>
-
         <input
+          type="text"
+          value={interaction.attendees || ""}
           readOnly
           placeholder="AI will detect attendees..."
           className="w-full rounded-lg border bg-gray-50 p-3"
         />
       </div>
 
+      {/* Location */}
       <div className="mt-5">
         <label className="mb-2 block text-sm font-medium">
           Location
         </label>
-
         <input
+          type="text"
+          value={interaction.location || ""}
           readOnly
           placeholder="Hospital / Clinic / Online"
           className="w-full rounded-lg border bg-gray-50 p-3"
         />
       </div>
 
+      {/* Topics Discussed */}
       <div className="mt-5">
         <label className="mb-2 block text-sm font-medium">
           Topics Discussed
         </label>
-
         <textarea
           rows={4}
+          value={interaction.topics || ""}
           readOnly
           placeholder="AI will summarize the discussion..."
           className="w-full rounded-lg border bg-gray-50 p-3"
